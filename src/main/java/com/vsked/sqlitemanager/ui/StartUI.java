b56340874d1fb510aaff77e81dea3de4372cd50a
bruce <vsked@163.com>
2025-03-25 14:48:12 +0800

    Update StartUI.java
    move data to new computer
    change startUI exapmle path

--- src/main/java/com/vsked/sqlitemanager/ui/StartUI.java
+++ src/main/java/com/vsked/sqlitemanager/ui/StartUI.java
@@ -2,7 +2,7 @@ package com.vsked.sqlitemanager.ui;
 
 public class StartUI {
     //D:\dellm4600\L\git\vskedsqlitemanager\myTestSqlite01.db
-    //D:\vsked\project\vskedsqlitemanager\myTestSqlite01.db
+    //C:\vsked\project\vskedsqlitemanager\myTestSqlite01.db
     public static void main(String[] args) {
         ApplicationMainUI.main(args);
     }
