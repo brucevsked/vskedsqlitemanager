26cb87ca9da89e7ea0a16830631f3f59edd46419
bruce <vsked@163.com>
2025-09-05 09:10:00 +0800

    update pom add jdkversion
    update pom use jdk21
    update maven shade and jar pluging

--- src/main/java/com/vsked/jpa/repositoryimp/RoleJpaRepositoryImpl.java
+++ src/main/java/com/vsked/jpa/repositoryimp/RoleJpaRepositoryImpl.java
@@ -1,54 +1,54 @@
-package com.vsked.jpa.repositoryimp;
-
-import com.vsked.jpa.po.MenuPo;
-import com.vsked.jpa.po.RolePo;
-import com.vsked.system.domain.Role;
-import com.vsked.system.repository.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import java.util.LinkedList;
-import java.util.List;
-
-@Repository
-public class RoleJpaRepositoryImpl implements RoleRepository {
-    @Autowired
-    MenuJpaRepositoryImpl menuJpaRepositoryImpl;
-
-    public Role poToRole(RolePo po){
-        if(po==null){
-            return null;
-        }
-        Long id=po.getId();
-        String name=po.getName();
-        List<Menu> menus=menuJpaRepositoryImpl.posToMenus(po.getMenus());
-        return new Role(id,name,menus);
-    }
-
-    public RolePo roleToPo(Role role){
-        if(role==null){
-            return null;
-        }
-        RolePo po=new RolePo();
-        po.setId(role.getId());
-        po.setName(role.getName());
-        List<MenuPo> menuPos=menuJpaRepositoryImpl.menusToPos(role.getMenus());
-        po.setMenus(menuPos);
-        return po;
-    }
-
-    public List<Role> posToRoles(List<RolePo> pos){
-        List<Role> datas=new LinkedList<>();
-        for(RolePo po:pos){
-            datas.add(poToRole(po));
-        }
-        return datas;
-    }
-
-    public List<RolePo> rolesToPos(List<Role> roles){
-        List<RolePo> datas=new LinkedList<>();
-        for(Role role:roles){
-            datas.add(roleToPo(role));
-        }
-        return datas;
-    }
-}
+//package com.vsked.jpa.repositoryimp;
+//
+//import com.vsked.jpa.po.MenuPo;
+//import com.vsked.jpa.po.RolePo;
+//import com.vsked.system.domain.Role;
+//import com.vsked.system.repository.RoleRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Repository;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//@Repository
+//public class RoleJpaRepositoryImpl implements RoleRepository {
+//    @Autowired
+//    MenuJpaRepositoryImpl menuJpaRepositoryImpl;
+//
+//    public Role poToRole(RolePo po){
+//        if(po==null){
+//            return null;
+//        }
+//        Long id=po.getId();
+//        String name=po.getName();
+//        List<Menu> menus=menuJpaRepositoryImpl.posToMenus(po.getMenus());
+//        return new Role(id,name,menus);
+//    }
+//
+//    public RolePo roleToPo(Role role){
+//        if(role==null){
+//            return null;
+//        }
+//        RolePo po=new RolePo();
+//        po.setId(role.getId().getId());
+//        po.setName(role.getName().getName());
+//        List<MenuPo> menuPos=menuJpaRepositoryImpl.menusToPos(role.getMenus());
+//        po.setMenus(menuPos);
+//        return po;
+//    }
+//
+//    public List<Role> posToRoles(List<RolePo> pos){
+//        List<Role> datas=new LinkedList<>();
+//        for(RolePo po:pos){
+//            datas.add(poToRole(po));
+//        }
+//        return datas;
+//    }
+//
+//    public List<RolePo> rolesToPos(List<Role> roles){
+//        List<RolePo> datas=new LinkedList<>();
+//        for(Role role:roles){
+//            datas.add(roleToPo(role));
+//        }
+//        return datas;
+//    }
+//}
--- src/main/java/com/vsked/jpa/repositoryimp/UserJpaRepositoryImpl.java
+++ src/main/java/com/vsked/jpa/repositoryimp/UserJpaRepositoryImpl.java
@@ -1,66 +1,64 @@
-package com.vsked.jpa.repositoryimp;
-
-
-import com.vsked.jpa.po.RolePo;
-import com.vsked.jpa.po.UserPo;
-import com.vsked.jpa.repository.UserRepositoryJPA;
-import com.vsked.system.domain.Role;
-import com.vsked.system.domain.User;
-import com.vsked.system.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
-@Repository
-public class UserJpaRepositoryImpl implements UserRepository {
-
-    @Autowired
-    UserRepositoryJPA userRepositoryJPA;
-    @Autowired
-    RoleJpaRepositoryImpl roleJpaRepositoryImpl;
-
-    public User save(User user) {
-        UserPo po=userToPo(user);
-        UserPo savePo=userRepositoryJPA.save(po);
-        return poToUser(savePo);
-    }
-
-
-    public User findByUserName(String name) {
-        UserPo po=userRepositoryJPA.findByName(name).orElse(null);
-        return poToUser(po);
-    }
-
-    public User findByCertificateId(Long certificateId) {
-        UserPo po=userRepositoryJPA.findByCertificateId(certificateId).orElse(null);
-        return poToUser(po);
-    }
-
-    public User poToUser(UserPo po){
-        if(po==null){
-            return null;
-        }
-        Long id=po.getId();
-        String userName=po.getName();
-        String password=po.getPassword();
-        boolean accountNonExpired=po.isAccountNonExpired();
-        boolean accountNonLock=po.isAccountNonLock();
-        boolean credentialsNonExpired=po.isCredentialsNonExpired();
-        boolean enable=po.isEnable();
-        List<Role> roles=roleJpaRepositoryImpl.posToRoles(po.getRoles());
-        return new User(id,userName,password,accountNonExpired,accountNonLock,credentialsNonExpired,enable,roles);
-    }
-
-    public UserPo userToPo(User user){
-        if(user==null){
-            return null;
-        }
-
-        UserPo po=new UserPo();
-        po.setId(user.getId().getId());
-        po.setName(user.getName().getName());
-        po.setEnable(user.isEnabled());
-        List<RolePo> roles=roleJpaRepositoryImpl.rolesToPos(user.getRoles());
-        return po;
-    }
-}
+//package com.vsked.jpa.repositoryimp;
+//
+//import com.vsked.jpa.po.RolePo;
+//import com.vsked.jpa.po.UserPo;
+//import com.vsked.jpa.repository.UserRepositoryJPA;
+//import com.vsked.system.domain.Role;
+//import com.vsked.system.domain.User;
+//import com.vsked.system.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Repository;
+//import java.util.List;
+//
+//@Repository
+//public class UserJpaRepositoryImpl implements UserRepository {
+//
+//    @Autowired
+//    UserRepositoryJPA userRepositoryJPA;
+//    @Autowired
+//    RoleJpaRepositoryImpl roleJpaRepositoryImpl;
+//
+//    public User save(User user) {
+//        UserPo po=userToPo(user);
+//        UserPo savePo=userRepositoryJPA.save(po);
+//        return poToUser(savePo);
+//    }
+//
+//
+//    public User findByUserName(String name) {
+//        UserPo po=userRepositoryJPA.findByName(name).orElse(null);
+//        return poToUser(po);
+//    }
+//
+//    public User findByCertificateId(Long certificateId) {
+//        UserPo po=userRepositoryJPA.findByCertificateId(certificateId).orElse(null);
+//        return poToUser(po);
+//    }
+//
+//    public User poToUser(UserPo po){
+//        if(po==null){
+//            return null;
+//        }
+//        Long id=po.getId();
+//        String userName=po.getName();
+//        String password=po.getPassword();
+//        boolean accountNonExpired=po.isAccountNonExpired();
+//        boolean accountNonLock=po.isAccountNonLock();
+//        boolean credentialsNonExpired=po.isCredentialsNonExpired();
+//        boolean enable=po.isEnable();
+//        List<Role> roles=roleJpaRepositoryImpl.posToRoles(po.getRoles());
+//        return new User(id,userName,password,accountNonExpired,accountNonLock,credentialsNonExpired,enable,roles);
+//    }
+//
+//    public UserPo userToPo(User user){
+//        if(user==null){
+//            return null;
+//        }
+//
+//        UserPo po=new UserPo();
+//        po.setId(user.getId().getId());
+//        po.setName(user.getName().getName());
+//        List<RolePo> roles=roleJpaRepositoryImpl.rolesToPos(user.getRoles());
+//        return po;
+//    }
+//}
