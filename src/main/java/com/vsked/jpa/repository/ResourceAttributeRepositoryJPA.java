daed7c78a01dd3e00420307f7d3981e57a036d42
bruce <vsked@163.com>
2025-09-18 10:14:55 +0800

    add resource attribute and resource spring data jpa implements
    update package name from repository imp to repository jpa implements
    add resource and resource attribute repository

---
+++ src/main/java/com/vsked/jpa/repository/ResourceAttributeRepositoryJPA.java
@@ -0,0 +1,7 @@
+package com.vsked.jpa.repository;
+
+import com.vsked.jpa.po.ResourceAttributePO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceAttributeRepositoryJPA extends JpaRepository<ResourceAttributePO,Long> {
+}
---
+++ src/main/java/com/vsked/jpa/repositoryjpaimp/ResourceAttributeRepositoryImpl.java
@@ -0,0 +1,71 @@
+package com.vsked.jpa.repositoryjpaimp;
+
+
+import com.vsked.jpa.po.ResourceAttributePO;
+import com.vsked.jpa.repository.ResourceAttributeRepositoryJPA;
+import com.vsked.system.domain.ResourceAttribute;
+import com.vsked.system.domain.ResourceAttributeId;
+import com.vsked.system.domain.ResourceAttributeName;
+import com.vsked.system.repository.ResourceAttributeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ResourceAttributeRepositoryImpl implements ResourceAttributeRepository {
+
+    @Autowired
+    ResourceAttributeRepositoryJPA resourceAttributeSpringDataJPARepository;
+
+    public ResourceAttribute getBy(ResourceAttributeId resourceAttributeId){
+        ResourceAttributePO po=getOptionalBy(resourceAttributeId.getId()).get();
+        return poToResourceAttribute(po);
+    }
+
+    public Boolean getIsExistBy(ResourceAttributeId resourceAttributeId) {
+        return getOptionalBy(resourceAttributeId.getId()).isPresent();
+    }
+
+    public Optional<ResourceAttributePO> getOptionalBy(Long resourceAttributeId) {
+        return resourceAttributeSpringDataJPARepository.findById(resourceAttributeId);
+    }
+
+    public void save(ResourceAttribute resourceAttribute) {
+        ResourceAttributePO po=resourceAttributeToPo(resourceAttribute);
+        resourceAttributeSpringDataJPARepository.save(po);
+    }
+
+    public Long nextResourceAttributeId() {
+        return resourceAttributeSpringDataJPARepository.count()+10000000000000000L+1;
+    }
+
+    public ResourceAttribute poToResourceAttribute(ResourceAttributePO po){
+        ResourceAttributeId id=new ResourceAttributeId(po.getId());
+        ResourceAttributeName name=new ResourceAttributeName(po.getName());
+        return new ResourceAttribute(id,name);
+    }
+
+    public ResourceAttributePO resourceAttributeToPo(ResourceAttribute resourceAttribute){
+        Long id=resourceAttribute.getId().getId();
+        String name=resourceAttribute.getName().getName();
+        return new ResourceAttributePO(id,name);
+    }
+
+    public List<ResourceAttribute> posToResourceAttributes(List<ResourceAttributePO> pos){
+        List<ResourceAttribute> ras=new LinkedList<>();
+        for(ResourceAttributePO po:pos){
+            ras.add(poToResourceAttribute(po));
+        }
+        return ras;
+    }
+
+    public List<ResourceAttributePO> resourceAttributesToPos(List<ResourceAttribute> ras){
+        List<ResourceAttributePO> pos=new LinkedList<>();
+        for(ResourceAttribute attribute:ras){
+            pos.add(resourceAttributeToPo(attribute));
+        }
+        return pos;
+    }
+}
